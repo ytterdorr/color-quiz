9066fc06e86000ef5e74ce8fccf78975d9cde208
Tore Haglund <tore.haglund@gmail.com>
2023-09-23 13:33:28 +0200

    Three game states

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { Button } from "@mantine/core"
+import { useState } from "react"
+
 export default function App() {
     // TODOs
     // Game state, start, play, finish
+    const [gameState, setGameState] = useState<'start' | 'play' | 'finish'>('start')
     return (
         <>
-            <p>App</p>
+            {gameState === 'start' && (
+                <>
+                    <h1>Start</h1>
+                    <Button onClick={() => setGameState('play')}>Play</Button>
+                </>
+            )}
+            {gameState === 'play' && (
+                <>
+                    <h1>Play</h1>
+                    <Button onClick={() => setGameState('finish')}>Finish</Button>
+                </>
+            )}
+            {gameState === 'finish' && (
+                <>
+                    <h1>Finish</h1>
+                    <Button onClick={() => setGameState('start')}>To Start</Button>
+                </>
+            )}
         </>
     )
 }
