b3dd8a58850e4577ef6b5f39815f6fd6821782c1
Tore Haglund <tore.haglund@gmail.com>
2023-09-23 14:45:59 +0200

    Add more colorsets

--- src/assets/basic-colors.ts
+++ src/assets/basic-colors.ts
@@ -24,3 +24,45 @@ export const ThreeColors = {
 }
 
 export type BasicColorKey = keyof typeof BasicColors;
+
+export const Reds = {
+    "maroon": "#800000",
+    "darkred": "#8B0000",
+    "brown": "#A52A2A",
+    "firebrick": "#B22222",
+    "crimson": "#DC143C",
+    "red": "#FF0000",
+    "tomato": "#FF6347",
+    "coral": "#FF7F50",
+    "indianred": "#CD5C5C",
+    "lightcoral": "#F08080",
+    "darksalmon": "#E9967A",
+    "salmon": "#FA8072",
+    "lightsalmon": "#FFA07A",
+    "orangered": "#FF4500",
+    "darkorange": "#FF8C00",
+    "orange": "#FFA500",
+    "gold": "#FFD700",
+}
+
+export const yellows = {
+    "orangered": "#FF4500",
+    "darkorange": "#FF8C00",
+    "orange": "#FFA500",
+    "gold": "#FFD700",
+    "darkgoldenrod": "#B8860B",
+    "goldenrod": "#DAA520",
+    "palegoldenrod": "#EEE8AA",
+    "darkkhaki": "#BDB76B",
+    "khaki": "#F0E68C",
+    "olive": "#808000",
+    "yellow": "#FFFF00",
+    "yellowgreen": "#9ACD32",
+    "darkolivegreen": "#556B2F",
+    "olivedrab": "#6B8E23",
+    "lawngreen": "#7CFC00",
+    "chartreuse": "#7FFF00",
+}
+
+export const Greens = {
+}
